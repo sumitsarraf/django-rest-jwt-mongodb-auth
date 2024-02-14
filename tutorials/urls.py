40ca05ff022ef068fbed6bcf3f016ee746290cb5
Sumit Ranjan <34721357+sumitsarraf@users.noreply.github.com>
2024-02-14 15:29:17 +0530

    initial commit

---
+++ tutorials/urls.py
@@ -0,0 +1,8 @@
+from django.urls import re_path as url
+from tutorials import views
+ 
+urlpatterns = [ 
+    url(r'^api/tutorials$', views.tutorial_list),
+    url(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail),
+    url(r'^api/tutorials/published$', views.tutorial_list_published),
+]
